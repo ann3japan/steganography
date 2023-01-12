c6f1a7533a2a0d58ca541a03836762ec7f72fa1e
Anna <70053686+ann3japan@users.noreply.github.com>
2023-01-12 11:47:31 +0300

    update src.py

--- steganography/src.py
+++ steganography/src.py
@@ -8,8 +8,6 @@ c = {}
 for i in range(255):
     d[chr(i)] = i
     c[i] = chr(i)
-  
-
 
 x = cv2.imread("image.jpg")
 
@@ -17,8 +15,8 @@ i = x.shape[0]
 j = x.shape[1]
 print(i, j)
 
-key = input("Enter key to edit(Security Key) : ")
-text = input("Enter text to hide : ")
+key = input("Enter key to edit: ")
+text = input("Enter text to hide: ")
 
 kl = 0
 tln = len(text)
@@ -37,8 +35,7 @@ for i in range(l):
     
 cv2.imwrite("encrypted_image.jpg", x) 
 os.startfile("encrypted_image.jpg")
-print("Data hidden successfully.")
-    
+print("Data hidden successfully")
 
 kl = 0
 tln = len(text)
@@ -46,10 +43,10 @@ z = 0
 n = 0  
 m = 0  
 
-ch = int(input("\nEnter 1 to extract data from Image : "))
+ch = int(input("\nEnter 1 to extract data from image: "))
 
 if ch == 1:
-    key1 = input("\n\nRe-enter key to extract text : ")
+    key1 = input("\n\nRe-enter key to extract text: ")
     decrypt = ""
 
     if key == key1 :
@@ -59,9 +56,6 @@ if ch == 1:
             m = m + 1
             m = (m + 1) % 3
             kl = (kl + 1) % len(key)
-        print("Encrypted text was : ", decrypt)
+        print("Encrypted text was: ", decrypt)
     else:
-        print("Key doesn't matched.")
-else:
-    print("Thank you.")
-   
+        print("Key does not matched")
